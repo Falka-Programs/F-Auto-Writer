71a0a70274a170d330f7087c761f38fe0fb1420a
Feshior <vladislavshvetsua@gmail.com>
2022-01-24 17:17:24 +0200

    first commit

---
+++ AutoWriter/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoWriter
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageSender ms = new MessageSender();
+            Process[] processes = Process.GetProcessesByName("firefox");
+            try
+            {
+                ms.SendMessage("Hello", processes);
+            }catch(ArgumentException)
+            {
+                MessageBox.Show($"Procces is invalid!","Warning",MessageBoxButton.OK,MessageBoxImage.Warning);
+            }catch(Exception ex)
+            {
+                MessageBox.Show($"Unknow error!\r\n{ex.Message}","Error",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+            //Process[] processes = Process.GetProcessesByName("firefox");
+            //MessageBox.Show($"Find {processes.Length} process");
+
+            //foreach (Process proc in processes)
+            //    PostMessage(proc.MainWindowHandle, WM_KEYDOWN, VK_F5, 0);
+        }
+    }
+}
---
+++ AutoWriter/MessageSender.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoWriter
+{
+    
+    public class MessageSender
+    {
+        private class SymbolInVm
+        {
+            public char symbol { get; set; }
+            public int value { get; set; }
+            public SymbolInVm(char sym,int val){
+                symbol = sym;
+                value = val;
+            }
+        }
+        [DllImport("user32.dll")]
+        private static extern int SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
+        private List<SymbolInVm> symbols;
+
+		const UInt32 WM_KEYDOWN = 0x0100;
+        const int VK_RETURN = 0x0D;
+
+        private void FillSymbols()
+        {
+            symbols = new List<SymbolInVm>();
+            char[] alph = { 'A','B','C','D', 'E', 'F', 'G', 'H' , 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q','R','S','T','U','V','W','X','Y','Z' };
+            int temp = 65;
+            for(int i=0; i<alph.Length; i++)
+            {
+                symbols.Add(new SymbolInVm(alph[i], temp++));
+            }
+        }
+
+        public void SendMessage(string message, Process[] processes)
+        {
+            if (processes.Length == 0)
+            {
+                throw new ArgumentException("Wrong processes");
+            }
+            else
+            {
+                //foreach(var procces in )
+            }
+        }
+
+        public MessageSender()
+        {
+            FillSymbols();
+        }
+    }
+}
