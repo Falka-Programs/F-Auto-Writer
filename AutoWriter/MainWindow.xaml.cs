c9539217ac796533c2b358db8c491c2307d695b5
Feshior <vladislavshvetsua@gmail.com>
2022-01-26 21:41:19 +0200

    Design development done

--- AutoWriter/MainWindow.xaml.cs
+++ AutoWriter/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,33 +24,86 @@ namespace AutoWriter
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     /// 
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        int timeout;
+        public int Timeout {
+            get {
+                return timeout;
+            }
+            set
+            {
+                if (value < 0 || value > 3000)
+                {
+                    MessageBox.Show("Wrong value.\r\n Value must be beetwen 5 and 3000","Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+                else
+                {
+                    timeout = value;
+                    OnProperyChanged("Timeout");
+                }
+            }
+        }
+
+        Process[] Processes { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
-            
+            DataContext = this;
+            Timeout = 30;
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageSender ms = new MessageSender();
-            Process[] processes = Process.GetProcessesByName("firefox");
-            try
+
+            string pName = ProccesTextBox.Text;
+            Processes = Process.GetProcessesByName(pName);
+
+            if(Processes.Length == 0)
             {
-                ms.SendMessage("Hello", processes);
-            }catch(ArgumentException)
-            {
-                MessageBox.Show($"Procces is invalid!","Warning",MessageBoxButton.OK,MessageBoxImage.Warning);
-            }catch(Exception ex)
-            {
-                MessageBox.Show($"Unknow error!\r\n{ex.Message}","Error",MessageBoxButton.OK,MessageBoxImage.Error);
+                processCountLabel.Content = "NF";
+                processCountLabel.Foreground = Brushes.Red;
             }
+            else
+            {
+                processCountLabel.Content = Processes.Length;
+                processCountLabel.Foreground = Brushes.LightGreen;
+            }
+
+            //MessageSender ms = new MessageSender();
+            //Thread.Sleep(1000);
+            //try
+            //{
+            //    ms.SendMessage("Hello", processes);
+            //    MessageBox.Show("Sended!", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            //}
+            //catch (ArgumentException)
+            //{
+            //    MessageBox.Show($"Procces is invalid!", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show($"Unknow error!\r\n{ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            //}
+
             //Process[] processes = Process.GetProcessesByName("firefox");
             //MessageBox.Show($"Find {processes.Length} process");
 
             //foreach (Process proc in processes)
             //    PostMessage(proc.MainWindowHandle, WM_KEYDOWN, VK_F5, 0);
+        }
+
+
+        //Property changed implement
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnProperyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private void FileSelectButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- AutoWriter/MessageSender.cs
+++ AutoWriter/MessageSender.cs
@@ -6,36 +6,72 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
+using WindowsInput.Native;
+using WindowsInput;
+
 namespace AutoWriter
 {
     
     public class MessageSender
     {
+        [DllImport("user32.dll")]
+        private static extern int SetForegroundWindow(IntPtr hWnd);
+
+        InputSimulator sim = new InputSimulator();
+
+        public void SendMessage(string message, Process[] processes)
+        {
+
+            message = message.ToUpper();
+
+            if (processes.Length == 0) 
+                throw new ArgumentException("Wrong processes");
+            else
+            {
+                // Console.WriteLine(processes.Length);
+                Process process = processes[0];
+                //foreach(var process in processes)
+                //{
+
+                for (int i = 0; i < message.Length; i++)
+                {
+                    try
+                    {
+                            SetForegroundWindow(process.MainWindowHandle);
+                            sim.Keyboard.KeyPress((VirtualKeyCode)FindValue(message[i]));
+                        // Console.WriteLine("MEssage posted!");
+                    }
+                    catch (Exception e)
+                    {
+                        System.Windows.MessageBox.Show($"Symbol {message[i]} not finded. Only english letter is allowed!\r\nSymbol skipped", "Error");
+                    }
+
+                }
+                sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+                //}
+            }
+        }
+
         private class SymbolInVm
         {
             public char symbol { get; set; }
             public int value { get; set; }
-            public SymbolInVm(char sym,int val){
+            public SymbolInVm(char sym, int val)
+            {
                 symbol = sym;
                 value = val;
             }
         }
-        [DllImport("user32.dll")]
-        private static extern int SetForegroundWindow(IntPtr hWnd);
 
-        [DllImport("user32.dll")]
-        private static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
-        private List<SymbolInVm> symbols;
+        
 
-		const UInt32 WM_KEYDOWN = 0x0100;
-        const int VK_RETURN = 0x0D;
-
+              private List<SymbolInVm> symbols;
         private void FillSymbols()
         {
             symbols = new List<SymbolInVm>();
-            char[] alph = { 'A','B','C','D', 'E', 'F', 'G', 'H' , 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q','R','S','T','U','V','W','X','Y','Z' };
+            char[] alph = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
             int temp = 65;
-            for(int i=0; i<alph.Length; i++)
+            for (int i = 0; i < alph.Length; i++)
             {
                 symbols.Add(new SymbolInVm(alph[i], temp++));
             }
@@ -45,42 +81,17 @@ namespace AutoWriter
         {
             for (int i = 0; i < symbols.Count; i++)
             {
-                if(symbols[i].symbol == sym)
+
+                if (symbols[i].symbol == sym)
                 {
+                    Console.WriteLine($"VALUE SENDED {sym}");
                     return symbols[i].value;
                 }
             }
             throw new ArgumentOutOfRangeException("Not finded");
         }
 
-        public void SendMessage(string message, Process[] processes)
-        {
-            if (processes.Length == 0)
-            {
-                throw new ArgumentException("Wrong processes");
-            }
-            else
-            {
-                foreach(var process in processes)
-                {
-                    int keyCode = 0;
-                    for(int i = 0; i < message.Length; i++)
-                    {
-                        try
-                        {
-                            keyCode = FindValue(message[i]);
-                            PostMessage(process.MainWindowHandle, WM_KEYDOWN, keyCode, 0);
-                        }
-                        catch(Exception e)
-                        {
-                            System.Windows.MessageBox.Show($"Symbol {message[i]} not finded. Only english letter is allowed!\r\nSymbol skipped","Error");
-                        }
-                        
-                    }
-                    PostMessage(process.MainWindowHandle, WM_KEYDOWN, VK_RETURN, 0);
-                }
-            }
-        }
+        
 
         public MessageSender()
         {
